ffa42258214be8de7f1c839ee4ef84469ac7b61f
dertlimehmetsalih286-cyber <dertlimehmetsalih286@gmail.com>
2026-05-07 16:31:38 +0300

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  7 12:36:58 2026
-
-@author: Dell
-"""
-
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
-import * as schema from "./schema";
+import { drizzle } from 'drizzle-orm/node-postgres';
+import pg from 'pg';
 
 const { Pool } = pg;
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL must be set.");
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle(pool, { schema });
-export * from "./schema";
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL tanimlanmamis!");
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // RENDER İÇİN KRİTİK AYAR:
+  ssl: {
+    rejectUnauthorized: false // Bu satır bağlantıdaki SSL sertifika hatasını çözer
+  }
+});
+
+export const db = drizzle(pool);
